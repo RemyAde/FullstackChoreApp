67a14bf05cfb58c60227971f91d460200d7e6d7d
RemyAde <remysamade@gmail.com>
2024-03-21 03:16:08 +0100

    UPDATE chore CRUD functions and authorization

--- BackEnd/routers/chores.py
+++ BackEnd/routers/chores.py
@@ -1,13 +1,14 @@
 import sys
 sys.path.append("..")
 
-from typing import Optional
-from fastapi import Depends, HTTPException, APIRouter, Request
+from starlette import status
+from starlette.responses import RedirectResponse
+from fastapi import Depends, APIRouter, Request, Form
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from pydantic import BaseModel, Field
-from .auth import get_current_user, get_user_exception
+
+from .auth import get_current_user
 
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -32,13 +33,102 @@ def get_db():
         db.close()
 
 @router.get("/", response_class=HTMLResponse)
-async def read_all_by_user(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
+
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+    
+    chores = db.query(models.Chores).filter(models.Chores.owner_id == user.get("id")).all()
+
+    return templates.TemplateResponse("home.html", {"request": request, "chores": chores, "user": user})
 
 @router.get("/add-chore", response_class=HTMLResponse)
 async def add_new_chore(request: Request):
-    return templates.TemplateResponse("add-chore.html", {"request": request})
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+    
+    return templates.TemplateResponse("add-chore.html", {"request": request, "user": user})
 
+@router.post("/add-chore", response_class=HTMLResponse)
+async def create_chore(request: Request, title: str = Form(...), description : str = Form(...),
+                       priority: int = Form(...), db:Session = Depends(get_db)):
+    
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+    
+    chore_model = models.Chores()
+    chore_model.title = title
+    chore_model.description = description
+    chore_model.priority = priority
+    chore_model.complete = False
+    chore_model.owner_id = user.get("id")
+
+    db.add(chore_model)
+    db.commit()
+
+    return RedirectResponse(url="/chores", status_code=status.HTTP_302_FOUND)
+    
 @router.get("/edit-chore/{chore_id}", response_class=HTMLResponse)
-async def edit_chore(request: Request):
-    return templates.TemplateResponse("edit-chore.html", {"request": request})
+async def edit_chore(request: Request, chore_id: int, db: Session = Depends(get_db)):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+
+    chore = db.query(models.Chores).filter(models.Chores.id == chore_id).first()
+    return templates.TemplateResponse("edit-chore.html", {"request": request, "chore": chore, "user": user})
+
+@router.post("/edit-chore/{chore_id}", response_class=HTMLResponse)
+async def edit_chore_commit(request: Request, chore_id: int, title: str = Form(...),
+                            description: str = Form(...), priority: int = Form(...),
+                            db:Session = Depends(get_db)):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+    
+    chore_model = db.query(models.Chores).filter(models.Chores.id == chore_id).first()
+
+    chore_model.title = title
+    chore_model.description = description
+    chore_model.priority = priority
+
+    db.add(chore_model)
+    db.commit()
+
+    return RedirectResponse(url="/chores", status_code=status.HTTP_302_FOUND)
+
+@router.get("/delete/{chore_id}")
+async def delete_chore(request: Request, chore_id: int, db: Session = Depends(get_db)):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+
+    chore_model = db.query(models.Chores).filter(models.Chores.id == chore_id)\
+        .filter(models.Chores.owner_id == user.get("id")).first()
+    
+    if chore_model is None:
+        return RedirectResponse(url="/chores", status_code=status.HTTP_302_FOUND)
+    
+    db.delete(chore_model)
+    db.commit()
+
+    return RedirectResponse(url="/chores", status_code=status.HTTP_302_FOUND)
+
+@router.get("/complete/{chore_id}", response_class=HTMLResponse)
+async def complete_chore(request: Request, chore_id: int, db: Session = Depends(get_db)):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+
+    chore = db.query(models.Chores).filter(models.Chores.id == chore_id).first()
+
+    chore.complete = not chore.complete 
+    #setting to the negation of current complete status 
+    # when user requests change
+
+    db.add(chore)
+    db.commit()
+
+    return RedirectResponse(url="/chores", status_code=status.HTTP_302_FOUND)
