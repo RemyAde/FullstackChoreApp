b7e8bb009be2efa8f5400b2ff9074eedde7b5e28
RemyAde <remysamade@gmail.com>
2024-03-21 03:16:49 +0100

    UPDATE uses router

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import auth, chores
+from routers import auth, chores, users
 from starlette.staticfiles import StaticFiles
+from starlette import status
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -10,5 +12,10 @@ models.Base.metadata.create_all(bind=engine)
 
 app.mount("/static", StaticFiles(directory="../FrontEnd/static"), name="static")
 
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/chores", status_code=status.HTTP_302_FOUND)
+
 app.include_router(auth.router)
+app.include_router(users.router)
 app.include_router(chores.router)
